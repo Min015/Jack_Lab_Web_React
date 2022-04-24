183be09c5b24330537e38901bbacf2fb3fdb6d3f
Min015 <maychen98059@gmail.com>
2022-04-24 17:11:44 +0800

    角色列表

--- src/Routes/admin/permission/PermissionManage.js
+++ src/Routes/admin/permission/PermissionManage.js
@@ -7,18 +7,24 @@ import CreateTable from './CreateTable';
 export default class PermissionManage extends Component {
     state = {
         table_header: [
-            "書名",
-            "作者",
-            "出版社",
-            "出版時間",
+            "角色名稱",
         ],
         object: [
             {
-                b_id: "a",
-                b_title: "秒懂行動網頁設計Visual Studio Code+GitHub+Bootstrap5+CSS3+HTML5+Web App專案實作",
-                b_author:"姜琇森 朱珮儀 章家源 董子瑜 蕭國倫 陳璟誼",
-                b_publisher:"碁峰",
-                b_time:"2021-12-15",
+                role_id: "a",
+                role_title: "管理者",
+            },
+            {
+                role_id: "b",
+                role_title: "研究生",
+            },
+            {
+                role_id: "c",
+                role_title: "專題生",
+            },
+            {
+                role_id: "c",
+                role_title: "訪客",
             },
         ]
     }
@@ -35,12 +41,25 @@ export default class PermissionManage extends Component {
                     <Sidebar />
                     <div className="content_in">
                         <div className="in">
-                            
+                            <div className="work">
+                                <div className="edit_button">
+                                    <div className="add">
+                                        <a href="#">
+                                            <p>刪除角色</p>
+                                        </a>
+                                    </div>
+                                    <div className="add">
+                                        <a href="#">
+                                            <p>新增角色</p>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <CreateTable table_header={table_header} table_content={object} />
                         </div>
                     </div>
                 </div>
             </div>
-
         )
     }
 }
