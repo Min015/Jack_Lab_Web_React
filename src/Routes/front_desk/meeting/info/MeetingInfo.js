e49b2460c558d45ef9e4d06aa46f52012b4d7d3e
Min015 <maychen98059@gmail.com>
2022-05-25 03:55:32 +0800

    改修改刪除順序&標籤顯示方式

--- src/Routes/front_desk/meeting/info/MeetingInfo.js
+++ src/Routes/front_desk/meeting/info/MeetingInfo.js
@@ -18,7 +18,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
 	return {
-		GET_MeetingInfo: (payload,callback) => dispatch(GET_MeetingInfo(payload,callback)),
+		GET_MeetingInfo: (payload, callback) => dispatch(GET_MeetingInfo(payload, callback)),
 		GET_MeetingDownload: (payload) => dispatch(GET_MeetingDownload(payload)),
 		DELETE_Meeting: (payload) => dispatch(DELETE_Meeting(payload)),
 	}
@@ -39,12 +39,12 @@ export default connect(mapStateToProps, mapDispatchToProps)(
 				Id: params.id,
 			})
 			const callback = (res) => {
-				console.log(42,res);
+				console.log(42, res);
 				this.setState({
-					meetinginfo:res,
+					meetinginfo: res,
 				})
 			}
-			this.props.GET_MeetingInfo(params.id,callback);
+			this.props.GET_MeetingInfo(params.id, callback);
 		}
 
 		//func
@@ -52,7 +52,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(
 			this.props.GET_MeetingDownload(filename);
 		}
 
-		Delete =(id) => {
+		Delete = (id) => {
 			this.props.DELETE_Meeting(id);
 			this.props.history.push("/meeting")
 		}
@@ -82,18 +82,28 @@ export default connect(mapStateToProps, mapDispatchToProps)(
 					<MemberLayout>
 						<div className="add_title">
 							<div className="title_name">
-								<h2>{MeetingInfo === undefined ? "" : MeetingInfo.Title}</h2>
-								<div className="tag">
-									{MeetingInfo === undefined ? "" : MeetingInfo.Tag?.map((item, index) => {
-										return (
-											<div key={index} className="small_tag">
-												{item.Name}
-											</div>
-										)
-									})}
-								</div>
+								<h2>
+									{MeetingInfo === undefined ? "" : MeetingInfo.Title}
+									<div className="tag">
+										{MeetingInfo === undefined ? "" : MeetingInfo.Tag?.map((item, index) => {
+											return (
+												<div key={index} className="small_tag">
+													{item.Name}
+												</div>
+											)
+										})}
+									</div>
+								</h2>
 							</div>
 							<div className="edit_button">
+								<div className="add">
+									<Link to={`/meeting/updatemeeting/${this.state.Id}`}>
+										<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M18.5118 2.99361L16.0177 0.489257C15.33 -0.201275 14.1663 -0.156091 13.4205 0.594632C12.6747 1.34349 12.626 2.51381 13.3156 3.20432L15.8097 5.70867C16.4974 6.39921 17.661 6.35405 18.4087 5.60331C19.1545 4.85259 19.2014 3.68603 18.5118 2.99361ZM2.61215 11.4456L7.60035 16.4543L15.7066 8.31654L10.7184 3.30784L2.61215 11.4456ZM0 19L6.5791 17.6773L1.31732 12.3938L0 19Z" fill="white" />
+										</svg>
+										<p>修改記錄</p>
+									</Link>
+								</div>
 								<div className="add">
 									<div
 										className='func_btn'
@@ -112,14 +122,6 @@ export default connect(mapStateToProps, mapDispatchToProps)(
 										<p>刪除記錄</p>
 									</div>
 								</div>
-								<div className="add">
-									<Link to={`/meeting/updatemeeting/${this.state.Id}`}>
-										<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-											<path d="M18.5118 2.99361L16.0177 0.489257C15.33 -0.201275 14.1663 -0.156091 13.4205 0.594632C12.6747 1.34349 12.626 2.51381 13.3156 3.20432L15.8097 5.70867C16.4974 6.39921 17.661 6.35405 18.4087 5.60331C19.1545 4.85259 19.2014 3.68603 18.5118 2.99361ZM2.61215 11.4456L7.60035 16.4543L15.7066 8.31654L10.7184 3.30784L2.61215 11.4456ZM0 19L6.5791 17.6773L1.31732 12.3938L0 19Z" fill="white" />
-										</svg>
-										<p>修改記錄</p>
-									</Link>
-								</div>
 							</div>
 						</div>
 						<div className="meeting">
@@ -131,7 +133,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(
 									<div className="col-3">
 										<p>時間</p>
 										<div className="info_block_content">
-											{MeetingInfo === undefined ? "" : MeetingInfo.Time.substr(0,10)}
+											{MeetingInfo === undefined ? "" : MeetingInfo.Time.substr(0, 10)}
 										</div>
 									</div>
 									<div className="col-3">
@@ -143,7 +145,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(
 									<div className="col-3">
 										<p>紀錄者</p>
 										<div className="info_block_content">
-											{MeetingInfo === undefined ? "" :MeetingInfo.Name}
+											{MeetingInfo === undefined ? "" : MeetingInfo.Name}
 										</div>
 									</div>
 								</div>
