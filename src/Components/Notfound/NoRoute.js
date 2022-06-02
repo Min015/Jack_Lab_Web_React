fdb7c25808f0a380f7b0861571804412c10f0f70
Min015 <maychen98059@gmail.com>
2022-06-02 18:13:43 +0800

    404

---
+++ src/Components/Notfound/NoRoute.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import GuestLayout from '../Layout/front/guest/GuestLayout';
+import './NoRoute.scss';
+export default class NoRoute extends Component {
+    render() {
+        return (
+            <GuestLayout>
+                <div id="NoRoute">
+                    <div className="center">
+                        <h1>404</h1>
+                        <div>
+                            您所尋找的路徑不存在<Link to='/index'>回主頁</Link>
+                        </div>
+                    </div>
+                </div>
+            </GuestLayout>
+        );
+    }
+}
