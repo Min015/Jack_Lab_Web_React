4a3f0accfffa82059ec959ee7ba103f735981cea
Min015 <maychen98059@gmail.com>
2022-06-05 23:51:10 +0800

    import React

--- src/Components/Header/front_end/GuestHeader.js
+++ src/Components/Header/front_end/GuestHeader.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
 import "./header1.scss";
