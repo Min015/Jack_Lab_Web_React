6a127553d19f5853c85575290aeaeb40bb119803
Min015 <maychen98059@gmail.com>
2022-06-05 21:30:56 +0800

    import React

--- src/Components/Notfound/NoRoute.js
+++ src/Components/Notfound/NoRoute.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import GuestLayout from '../Layout/front/guest/GuestLayout';
 import './NoRoute.scss';
